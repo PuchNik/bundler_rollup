53c4ebb655d6e2d47f70970fd58f9174a3e41092
PuchNik <i.p.chn.k@gmail.com>
2024-10-24 18:46:17 +0300

    add new project

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-function hello() {
-    console.log('Hi')
+import { sum } from "./main";
+import './styles.css'
+export image from './1.png'
+
+
+const array = [1, 2, 3].map(i => i + 1)
+
+function hello(...args) {
+    console.log('Hi', args[0], args[1])
 }
 
-hello()
+hello(sum(4, 4 ), array)
+
+
+const imageElement = document.createElement('img')
+imageElement.src = image
+document.querySelector('body').append(imageElement)
+
+
+console.log(image)
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,8 +1,24 @@
+import resolve from '@rollup/plugin-node-resolve'
+import babel from '@rollup/plugin-babel'
+import styles from 'rollup-plugin-styles'
+import serve from 'rollup-plugin-serve'
+import livereload from 'rollup-plugin-livereload'
+import image from 'rollup-plugin-image';
+
 export default {
     input: './index.js',
     output: {
         file: './dist/bundle.js',
         format: 'cjs',
-    }
+    },
+
+    plugins: [
+        resolve(),
+        babel({babelHelpers: 'bundled'}),
+        styles(),
+        image(),
+        serve(),
+        livereload(),
+    ]
 }
 
